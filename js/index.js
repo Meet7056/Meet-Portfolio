d70c0e19898f9ff90aec37f5d3fa0b11c9e03d35
Gohel Meet <124121423+Meet7056@users.noreply.github.com>
2023-03-10 12:51:18 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,114 @@
+
+function msg1() {
+
+    document.getElementById('heading').innerHTML='Trainee Software Developer';
+    document.getElementById('add1').innerHTML='Diligent Software developer with 1 month of experience in web development and web designing. Eager to join Sparrow SoftTech Company to build innovative and cutting-edge business solutions for the impressive suite of clients within its global reach.';
+    document.getElementById('add1').style.padding='9px';
+    document.getElementById('add2').innerHTML='In previous roles, slashed downtime by 25% and ensured 9 on-time project completion. Also identified and dealt with a significant process bottleneck that coding efficiency by 35% when resolved.';
+    document.getElementById('add2').style.padding='9px';
+
+    
+    document.getElementById("add4").style.display = "none";
+
+    
+    document.getElementById("add5").style.display = "none";
+
+    
+    document.getElementById("add6").style.display = "none";
+
+    
+    document.getElementById("add3").style.display = "none";
+
+    document.getElementById('_1').innerHTML='Trainee Software Developer';
+
+
+
+
+};
+
+
+
+function msg2() {
+
+    var str1 = "College: Sir, BPTI. Bhavnagar\nwith CGPI : 6.79\nin 2022"
+    var str2 = "School: Shree Arykul Higher Secondary School\nwith Percentile of 89.98\nin 2019"
+
+    
+// document.getElementsByClassName("about_details").style.transition = "all 2s";
+
+    
+    document.getElementById("heading").innerHTML = "Education";
+    document.getElementById("heading").style.paddingTop = "15px";
+    document.getElementById("heading").style.paddingBottom = "15px";
+    document.getElementById("heading").style.color= "aqua";
+
+    document.getElementById("add1").innerHTML = "Diploma in IT";
+    document.getElementById("add1").style.padding = "8px";
+    document.getElementById('add1').style.fontSize = "18px";
+    
+    document.getElementById("detail1").innerHTML = str1;
+    document.getElementById('detail1').style.fontSize = "15px";
+    document.getElementById('detail1').style.paddingTop = "0px";
+
+    document.getElementById("add2").innerHTML = "SSC";
+    document.getElementById("add2").style.padding = "8px";
+    document.getElementById('add2').style.fontSize = "18px";
+
+    document.getElementById("detail2").innerHTML = str2;
+    document.getElementById('detail2').style.fontSize = "15px";
+    document.getElementById('detail2').style.paddingTop = "0px";
+    
+    document.getElementById('detail3').style.display = "none";
+
+};
+
+
+function msg3() {
+
+    var str1 = "College: Sir, BPTI. Bhavnagar\nwith CGPI : 6.79\nin 2022"
+    var str2 = "School: Shree Arykul Higher Secondary School\nwith Percentile of 89.98\nin 2019"
+
+    document.getElementById("heading").innerHTML = "Skills";
+    document.getElementById("heading").style.paddingTop = "15px";
+    document.getElementById("heading").style.paddingBottom = "15px";
+    document.getElementById("heading").style.color= "aqua";
+
+    document.getElementById("add1").innerHTML = "HTML";
+    document.getElementById("add1").style.padding = "8px";
+    document.getElementById('add1').style.fontSize = "15px";
+    
+    document.getElementById('detail1').style.display = "none";
+
+    document.getElementById("add2").innerHTML = "CSS";
+    document.getElementById("add2").style.paddingLeft = "8px";
+    document.getElementById("add2").style.paddingTop = "1px";
+    document.getElementById('add2').style.fontSize = "15px";
+
+    
+    document.getElementById("add3").innerHTML = "JavaScript";
+    document.getElementById("add3").style.paddingLeft = "8px";
+    document.getElementById("add3").style.paddingTop = "1px";
+    document.getElementById('add3').style.fontSize = "15px";
+
+    
+    document.getElementById("add4").innerHTML = "Basic PHP";
+    document.getElementById("add4").style.paddingLeft = "8px";
+    document.getElementById("add4").style.paddingTop = "1px";
+    document.getElementById('add4').style.fontSize = "15px";
+
+    
+    document.getElementById("add5").innerHTML = "PhotoShop";
+    document.getElementById("add5").style.paddingLeft = "8px";
+    document.getElementById("add5").style.paddingTop = "1px";
+    document.getElementById('add5').style.fontSize = "15px";
+    
+    document.getElementById("add6").innerHTML = "Video Editing";
+    document.getElementById("add6").style.paddingLeft = "8px";
+    document.getElementById("add6").style.paddingTop = "1px";
+    document.getElementById('add6').style.fontSize = "15px";
+
+    document.getElementById('detail2').style.display = "none";
+    
+    document.getElementById('detail3').style.display = "none";
+
+};
